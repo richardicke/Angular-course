3f65ae99d08d21b2f262961b31c9e06ea70ea891
Richard Icke <r.icke@minddistrict.com>
2020-03-18 16:27:37 +0100

    WIP

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,9 @@
+export class Ingredient {
+    // public name: string;
+    // public amount: number;
+
+    // shorthand, use `public`:
+    constructor(public name: string, public amount: number) {
+
+    }
+}
