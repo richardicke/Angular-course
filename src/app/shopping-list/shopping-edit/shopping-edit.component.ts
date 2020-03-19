9ceca4b886711542870a3635b24725a363e4903b
Richard Icke <r.icke@minddistrict.com>
2020-03-19 14:25:21 +0100

    End of section 6

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,6 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild ('nameInput', {static: true}) nameInputRef: ElementRef;
+  @ViewChild ('amountInput', {static: true}) amountInputRef: ElementRef;
+  // @Output() ingredientAdded = new EventEmitter<{ name: string, amount: number }>();
+  // Instead of manually declaring the event payload type we can set it to the Ingredient model.
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+  onAddItem() {
+    // construct new Ingredient
+    const newIngredient = new Ingredient(
+      this.nameInputRef.nativeElement.value,
+      this.amountInputRef.nativeElement.value
+    )
+    this.ingredientAdded.emit(newIngredient);
+  }
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -14,6 +14,10 @@ export class ShoppingListComponent implements OnInit {
     new Ingredient('Tomatoes', 10)
   ];
 
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  };
+
   constructor() { }
 
   ngOnInit(): void {
